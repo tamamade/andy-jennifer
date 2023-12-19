48b9575b656d78539c0c3bffb4d72971c2bc527b
tamamade <139934063+tamamade@users.noreply.github.com>
2023-12-20 07:37:35 +0800

    Add files via upload

--- message.js
+++ message.js
@@ -5,6 +5,8 @@ const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
 const myAlert = document.querySelector(".alert-warning");
 const container = document.querySelector("#message-box");
+const statusKehadiran = document.querySelector(".status-kehadiran");
+
 const scriptURLGet =
   "https://script.googleusercontent.com/macros/echo?user_content_key=S1QLM2jFCK5K1fZDleY8cQucE4ZvJ44eyyYQIkx2h-L_IxGnypXjeaeG4hnA12bfijqBvKSM9AEMUzfa0gz__NGmPU6JkrT8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDKBGPRyCERUhrNvBLE_vI2yf5xa67PjQRYQDOhZ_4SyL7VwKS3eeoLwYhAKqpow5c3jCVF_JdC15C13WLs7us0bBZmdxEO81tz9Jw9Md8uu&lib=MsW59pOlHEHYIZnQl7KkJZD5VydodrvMG";
 
@@ -35,6 +37,7 @@ function fetchMessage() {
   fetch(scriptURLGet)
     .then((resp) => resp.json())
     .then((data) => {
+      statusKehadiran.innerHTML = `<small class="status-kehadiran">Hadir : ${data[0].jumlahhadir} | Tidak Hadir : ${data[0].jumlahtidakhadir} </small>`;
       maker(data);
     });
 }
@@ -42,19 +45,25 @@ function fetchMessage() {
 function maker(data) {
   let content = "";
   data.reverse().forEach((message) => {
-    content += `<div class="col-lg-7 col-md-10 col-12">
-              <div class="bubble-message d-flex align-items-center mb-3">
+    if (
+      message.nama.trim().length !== 0 &&
+      message.pesan.trim().length !== 0 &&
+      message.kehadiran.trim().length !== 0
+    ) {
+      content += `<div class="col-lg-7 col-md-10 col-12">
+      <div class="bubble-message d-flex align-items-center mb-3">
                 <div class="user-photo me-4">
                   <img src="assets/img/user-icon.svg" width="50px" />
-                </div>
+                  </div>
                 <div class="message ms-1">
                   <p class="fw-bolder">${message.nama} <span class="text-secondary" style="font-size:0.8rem">(${
-      message.kehadiran == "hadir" ? "Hadir" : "Tidak Hadir"
-    })</span></p>
+        message.kehadiran == "hadir" ? "Hadir" : "Tidak Hadir"
+      })</span></p>
                   <p>${message.pesan}</p>
                 </div>
-              </div>
-            </div>`;
+                </div>
+                </div>`;
+    }
   });
   container.innerHTML = content;
 }
